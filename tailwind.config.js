da25db6d6423eef9a2a283ca6135054abf8aba01
LuxOrionCoding <gungryan1234@gmail.com>
2025-10-11 16:31:19 +0800

    Adding custom font variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,13 @@ module.exports = {
       colors: {
         'custom-dark': '#020223',
         'primary': '#C8FA50',
-        'secondary': '#682FF5'
-      }
+        'secondary': '#682FF5',
+      },
+      fontFamily: {
+        heading: ['Sora', 'sans-serif'],
+        display: ['Urbanist', 'sans-serif'],
+        body: ['Poppins', 'sans-serif'],
+      },
     },
   },
   plugins: [],
