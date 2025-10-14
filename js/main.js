280a3910e84a106e041c3e10caacccc0222a6739
LuxOrionCoding <gungryan1234@gmail.com>
2025-10-14 10:14:08 +0800

    Refactoring components functionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 // Load reusable partials (navbar, footer)
-async function loadPartials() {
-    const includeElements = document.querySelectorAll("[data-include]");
-    for (const el of includeElements) {
-        const file = el.getAttribute("data-include");
-        const response = await fetch(file);
-        el.outerHTML = await response.text();
-    }
+async function loadPartials(navbar, footer) {
+    const includeNavbar = document.querySelector("[data-include-navbar]");
+    const includeFooter = document.querySelector("[data-include-footer]");
+
+    const navbarResponse = await fetch("partials/" + navbar);
+    const footerResponse = await fetch("partials/" + footer);
+
+    includeNavbar.outerHTML = await navbarResponse.text();
+    includeFooter.outerHTML = await footerResponse.text();
 }
 
 // Load the page inside #app
-async function loadPage(page) {
+async function loadPage(page, navbar = 'navbar.html', footer = 'footer.html') {
+    loadPartials(navbar, footer);
+
     const app = document.getElementById("app");
     const response = await fetch(`pages/${page}.html`);
     app.innerHTML = await response.text();
@@ -17,6 +21,5 @@ async function loadPage(page) {
 
 // Initialize app
 document.addEventListener("DOMContentLoaded", async () => {
-    await loadPartials();
     await loadPage("home"); // default page
 });
